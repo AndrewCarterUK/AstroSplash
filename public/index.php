0360fd66c28dc9c50971b63a5fb2f25ac77868e3
Andrew Carter <andrewcarter1992@gmail.com>
2015-10-06 00:29:15 +0100

    Container instantiated outside of front controller

--- public/index.php
+++ public/index.php
@@ -1,12 +1,8 @@
 <?php
 
-use Zend\ServiceManager\Config;
-use Zend\ServiceManager\ServiceManager;
-
 chdir(__DIR__.'/..');
 
 include 'vendor/autoload.php';
 
-$container = new ServiceManager(new Config(include 'config/container.php'));
-
+$container = include 'config/container.php';
 $container->get('Zend\\Expressive\\Application')->run();
