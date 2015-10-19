b146c9dd855e7a89980d85e2e52906f3d4b07df2
vagrant <vagrant@homestead>
2015-10-19 22:53:36 +0000

    Proper templates

--- templates/app/index.plates.php
+++ templates/app/index.plates.php
@@ -1,4 +1,77 @@
 <?php $this->layout('app::layout', ['title' => 'Astronomy Picture of the Day']) ?>
 
-<h1>Astronomy Picture of the Day App</h1>
-<p>Welcome to my Astronomy Picture of the Day App. It will use an API provided by NASA to deliver awesome astronomy pictures.</p>
+<h1 class="text-center">AstroSplash</h1>
+<p class="text-center">Images courtesy of NASA</p>
+
+<div id="gallery" class="row"></div>
+
+<div class="text-center">
+    <button id="more" class="btn btn-primary">More...</button><br /><br />
+</div>
+
+<div id="modal" class="modal fade">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 id="modal-title" class="modal-title"></h4>
+            </div>
+            <div class="modal-body">
+                <img id="modal-image" src="" alt="" class="img-responsive" />
+            </div>
+            <div class="modal-footer">
+                <p id="modal-caption"></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php $this->start('css') ?>
+    <style type="text/css">
+        body, html { background: #303030; color: white; }
+        #modal { color: black; }
+        #modal-caption { text-align: justify; }
+        #gallery .thumb { border: 1px solid white; margin-bottom: 1.5em; }
+    </style>
+<?php $this->stop() ?>
+
+<?php $this->start('javascript') ?>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        var requestInProgress = false;
+        var currentPage = 0;
+
+        $(document).ready(function () {
+            $('#more').click(function () {
+                if (requestInProgress) return;
+                requestInProgress = true;
+
+                $.get('/picture-list/' + currentPage, function (data) {
+                    currentPage++; requestInProgress = false;
+
+                    $.each(data, function (date, picture) {
+                        if (picture.media_type == 'image') {
+                            $('#gallery').append([
+                                '<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">',
+                                    '<a id="' + date + '" href="' + picture.url + '" data-lightbox="gallery" data-title="Hello">',
+                                        '<img src="' + picture.thumbnail_url + '" title="' + picture.title + '" class="img-responsive thumb" alt="" />',
+                                    '</a>',
+                                '</div>'
+                            ].join(''));
+    
+                            $('#' + date).click(function (event) {
+                                event.preventDefault();
+  
+                                $('#modal-title').text(picture.title);
+                                $('#modal-image').attr('src', picture.url);
+                                $('#modal-caption').text(picture.explanation);
+                                $('#modal').modal();
+                            });
+                        }
+                    });
+                });
+            }).click();
+        });
+    </script>
+<?php $this->stop() ?>
--- templates/app/layout.plates.php
+++ templates/app/layout.plates.php
@@ -2,9 +2,29 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title><?=$this->e($title);?></title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link type="text/css" rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" />
+
+        <title><?=$this->e($title)?></title>
+
+        <?=$this->section('javascript')?>
+        <?=$this->section('css')?>
+
+        <script type="text/javascript">
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-68480477-1', 'auto');
+            ga('send', 'pageview');
+        </script>
     </head>
     <body>
-        <?=$this->section('content')?>
+        <div class="container">
+            <?=$this->section('content')?>
+        </div>
     </body>
 </html>
